9aa3ce4070b9a0dd2d2656279519375f4408ccd6
Kiran-P-V <kiranpv82@gmail.com>
2024-04-08 12:12:34 +0530

    New changes

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
+const manifestForPlugIn = {
+  registerType: "prompt",
+  // includeAssests: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+  // manifest,
+};
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugIn)],
   server: {
     port: 5173,
+    host: true,
   },
   resolve: {
     alias: {
